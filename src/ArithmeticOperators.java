40b31c95f676f5dcc1ca28ae2c4cb0fbe3f58bab
Bryan Chaves <84508588+Bryncs@users.noreply.github.com>
2025-11-05 19:37:04 -0300

    Basic understanding of Arithmetic Operators in Java

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,36 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 3;
+        int z;
+
+        // Arithmetic Operators
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+        //System.out.println(z);
+
+        // Augmented Assignment Operators
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y; // Interger division
+        //x %= y;
+        // Increment and Decrement Operators
+        //x++;
+        //x++;
+        //x++;
+        //x--;
+        //x--;
+        //System.out.println(x);
+
+        // Order of Operations [P-E-M-D-A-S]
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+
+    }
+}
