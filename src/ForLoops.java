3791f2f90fba63760d388d9e98d90413e7720b53
Bryan Chaves <84508588+Bryncs@users.noreply.github.com>
2025-11-11 19:31:43 -0300

    For loops

---
+++ src/ForLoops.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ForLoops {
+    public static void main(String[] args) throws InterruptedException{
+
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+        System.out.print("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for(int i = 1; i <= max; i++){
+            System.out.println(i);
+        }
+         */
+
+        System.out.print("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
+
+        for(int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("HAPPY NEW YEAR!");
+
+        scanner.close();
+
+    }
+}
