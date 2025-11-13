394dca90e8c7089116c0cbd5d2519e4d699fdcf3
Bryan Chaves <84508588+Bryncs@users.noreply.github.com>
2025-11-13 19:39:40 -0300

    Variable Arguments

---
+++ src/VariableArguments.java
@@ -0,0 +1,32 @@
+public class VariableArguments {
+    public static void main(String[] args){
+
+        //System.out.println(add(1,2,3,4,5,6));
+        System.out.println(average(1,2,3,4,5,6,7));
+        System.out.println(average());
+
+    }
+    static int add(int... numbers){
+
+        int sum = 0;
+        for(int number : numbers){
+            sum += number;
+        }
+
+        return sum;
+    }
+    static double average(double... numbers){
+
+        double sum = 0;
+
+        if(numbers.length == 0){
+            return 0;
+        }
+
+        for(double number : numbers){
+            sum += number;
+        }
+
+        return sum / numbers.length;
+    }
+}
